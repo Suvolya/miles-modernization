3737482e0fef880a0378f0049945e1e18de7ac2f
Olga Suvorova <olga.suv.ya@yandex.ru>
2022-11-03 22:58:53 +0300

    change code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,8 @@
 public class BonusMilesService {
-    int miles;
 
     public int calculate(int cost) {
-        if (cost > 20) {
-            miles = cost / 20;
-        } else {
-            System.out.println("Сумма покупки меньше 20 руб.");
-        }
-        return miles;
+        int bonusMilesValue = cost/20;
+
+        return bonusMilesValue;
     }
 }
