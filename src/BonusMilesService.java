0e09eca62d0e9a4fda67268b3cfbf6dbf77128d3
Olga Suvorova <olga.suv.ya@yandex.ru>
2022-11-09 22:37:36 +0300

    add reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
 
     public int calculate(int cost) {
-        int bonusMilesValue = cost/20;
+        int bonusMilesValue = cost / 20;
 
         return bonusMilesValue;
     }
